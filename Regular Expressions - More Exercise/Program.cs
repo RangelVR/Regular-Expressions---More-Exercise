82848f45090ac8b11bbc03861642ceddb02b9471
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-19 23:39:57 +0200

    Update Program.cs

--- Regular Expressions - More Exercise/Program.cs
+++ Regular Expressions - More Exercise/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;
 
 string[] ticketsList = Console.ReadLine()
     .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
@@ -8,34 +8,34 @@ string pattern = @"(\@{6,}|\${6,}|\^{6,}|\#{6,})";
 
 foreach (string ticket in ticketsList)
 {
-	if (ticket.Length == 20)
-	{
-		Match left = Regex.Match(ticket.Substring(0, 10), pattern);
-		Match right = Regex.Match(ticket.Substring(10), pattern);
-		int minLength = Math.Min(left.Length, right.Length);
+    if (ticket.Length == 20)
+    {
+        Match left = Regex.Match(ticket.Substring(0, 10), pattern);
+        Match right = Regex.Match(ticket.Substring(10), pattern);
+        int minLength = Math.Min(left.Length, right.Length);
 
-		if (left.Success && right.Success)
-		{
-			string winLeft = left.Value.Substring(0, minLength);
-			string winRight = right.Value.Substring(0, minLength);
+        if (left.Success && right.Success)
+        {
+            string winLeft = left.Value.Substring(0, minLength);
+            string winRight = right.Value.Substring(0, minLength);
 
-			if (winLeft.Length + winRight.Length == 20)
-			{
+            if (winLeft.Length + winRight.Length == 20)
+            {
                 Console.WriteLine($"ticket \"{ticket}\" - {winLeft.Length}{winLeft[0]} Jackpot!");
             }
-			else
-			{
+            else
+            {
                 Console.WriteLine($"ticket \"{ticket}\" - {winLeft.Length}{winLeft[0]}");
             }
-		}
-		else
-		{
+        }
+        else
+        {
             Console.WriteLine($"ticket \"{ticket}\" - no match");
         }
 
-	}
-	else
-	{
+    }
+    else
+    {
         Console.WriteLine("invalid ticket");
     }
 }
